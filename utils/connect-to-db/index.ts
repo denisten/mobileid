4f92c8deede9fd3574d1977a7656ed71c7e93ea9
Тен Денис Сергеевич <dten@stream.ru>
2020-11-16 12:54:37 +0300

    added dockerfile

--- utils/connect-to-db/index.ts
+++ utils/connect-to-db/index.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb';
-
+const localURL = 'mongodb://den:denlikepro@127.0.0.1:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false'
 export const connectToDB = async () => {
-  const mongoClient = new MongoClient(process.env.DATA_BASE_URL, { useUnifiedTopology: true});
+  const mongoClient = new MongoClient(process.env.DATA_BASE_URL || localURL , { useUnifiedTopology: true});
   await mongoClient.connect()
   const db = mongoClient.db("Main");
   return db.collection("Main");
